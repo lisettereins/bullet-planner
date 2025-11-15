e252671bc90457037949b1282304027066c6a17f
lisettereins <lreins@tlu.ee>
2025-11-15 20:54:50 +0200

    feat: added action file for creating notes

---
+++ app/notes/action.ts
@@ -0,0 +1,19 @@
+'use server'
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function createNote(title: string, note: string) {
+  const supabase = await createClient();
+
+  const { error } = await supabase
+    .from("notes")
+    .insert({ title, note })
+    .select();
+    
+
+  if (error) {
+    throw new Error("create failed");
+  }
+  return { success: true };
+}
+

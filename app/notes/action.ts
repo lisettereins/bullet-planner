1e5de5a3141151665751264543a58fc19b1c8b5f
lisettereins <lreins@tlu.ee>
2025-12-01 18:40:22 +0200

    feat: update notes action file to fetch and insert user id into notes

--- app/notes/action.ts
+++ app/notes/action.ts
@@ -5,9 +5,25 @@ import { createClient } from '@/lib/supabase/server';
 export async function createNote(title: string, note: string, date: string) {
   const supabase = await createClient();
 
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError || !user) {
+    throw new Error('Not authenticated');
+  }
+
+  const userId = user.id;
+
   const { error } = await supabase
     .from('notes')
-    .insert({ title, note, date })
+    .insert({
+      title,
+      note,
+      date,
+      user_id: userId,
+    })
     .select();
 
   if (error) {
